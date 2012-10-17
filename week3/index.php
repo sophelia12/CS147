9f7bb45b07a7361bb368d0a144d97df7d15585c7
Sophie Burke <sophie.c.burke@gmail.com>
2012-10-17 13:49:32 -0700

    Pre Lab 4

--- week3/index.php
+++ week3/index.php
@@ -14,10 +14,23 @@
 	<body>
 	
 		<div class="banner"></div>
+		<?php
+		include("menu.php");
+		?>
 		
 		<table>
-		<!-- This is where we'll put all our content -->
-		
+		<?php
+		include("config.php");
+		$query = "select * from books";  //select all from books
+		// Don’t be intimidated by the following lines. You can pretty much
+		// always copy and paste these because they’re sort of like
+		// functional bits that never change.
+		$result = mysql_query($query);
+		while ($row = mysql_fetch_assoc($result)) {
+			echo "<p>".$row["title"]." by ".$row["author"]."</p>";
+			echo "<img src= '".$row["image"]."' />";
+		}
+		?>
 		</table>
 		
 		<script type="text/javascript">
