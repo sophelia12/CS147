9e45fb2a443c3904d7efbe8eab95efbe8e6ecf50
Sophie Burke <sophie.c.burke@gmail.com>
2012-10-24 14:30:56 -0700

    Lab 5

---
+++ starterkit/custom.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function() {
+	$("a").click(function() {
+     alert("Hello world!");
+   });
+   
+  	$(document).ready(function() {
+   		$("#orderedlist").addClass("red");
+ 	});
+ 	
+ 	$(document).ready(function() {
+   		$("#orderedlist > li").addClass("blue");
+ 	});
+ 	
+ 	$(document).ready(function() {
+   		$("#orderedlist li:last").hover(function() {
+     		$(this).addClass("green");
+   		},function(){
+     		$(this).removeClass("green");
+  		});
+ 	});
+});
